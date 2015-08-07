e947872f033317fc2fcea2b60e4a4abca4dcff18
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2015-08-07 09:53:31 +0200

    Range1 is outdated, should be Range

--- src/sgearray.jl
+++ src/sgearray.jl
@@ -2,12 +2,12 @@
 ## (c) 2014 David A. van Leeuwen
 ##
 
-issgearray() = haskey(ENV, "SGE_TASK_ID") && haskey(ENV, "SGE_TASK_LAST")
+issgearray() = haskey(ENV, "SGE_TASK_ID") && haskey(ENV, "SGE_TASK_LAST") && ENV["SGE_TASK_ID"] != "undefined"
 
 Base.start(sge::SGEArray) = sge.start
 Base.done(sge::SGEArray, state::Int) = state > length(sge.array) 
 Base.next(sge::SGEArray, state::Int) = sge.array[state], state+sge.step
 Base.length(sge::SGEArray) = div(length(sge.array)-sge.start, sge.step)+1
 
-SGEArray(r::Range1) = SGEArray([r])
+SGEArray(r::Range) = SGEArray([r])
 
