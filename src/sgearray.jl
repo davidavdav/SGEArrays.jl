aaa1b146b5d3befd69252b051d0245319c81cbc1
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2016-04-16 18:25:54 +0200

    Allow for general iterator, don't rely on length(iterator)

--- src/sgearray.jl
+++ src/sgearray.jl
@@ -1,12 +1,38 @@
 ## sgearray.jl.  Iterators for use in conjunction with Sun Grid Engine
-## (c) 2014 David A. van Leeuwen
+## (c) 2014, 2016 David A. van Leeuwen
 ##
 
 issgearray() = haskey(ENV, "SGE_TASK_ID") && haskey(ENV, "SGE_TASK_LAST") && ENV["SGE_TASK_ID"] != "undefined"
 
-Base.start(sge::SGEArray) = sge.start
-Base.done(sge::SGEArray, state::Int) = state > length(sge.array)
-Base.next(sge::SGEArray, state::Int) = sge.array[state], state+sge.step
-Base.length(sge::SGEArray) = div(length(sge.array)-sge.start, sge.step)+1
-
-SGEArray(r::Range) = SGEArray(collect(r))
+function Base.start(sge::SGEArray)
+    sge.state = start(sge.iterator)
+    state = 0
+    while true
+        if done(sge.iterator, sge.state)
+            state = -1
+            break
+        end
+        sge.next, sge.state = next(sge.iterator, sge.state)
+        if state % sge.step == sge.first
+            break
+        end
+        state += 1
+    end
+    return state
+end
+Base.done(sge::SGEArray, state::Int) = state < 0
+function Base.next(sge::SGEArray, state::Int)
+    item = sge.next
+    while true
+        if done(sge.iterator, sge.state)
+            state = -1
+            break
+        end
+        sge.next, sge.state = next(sge.iterator, sge.state)
+        state += 1
+        if state % sge.step == sge.first
+            break
+        end
+    end
+    return item, state
+end
--- src/sgetypes.jl
+++ src/sgetypes.jl
@@ -2,23 +2,26 @@
 ## (c) 2014 David A. van Leeuwen
 ##
 
-## The inner constructor returns the array itself if not running in
+## The inner constructor returns the iterator itself if not running in
 ## an SGE environment.  I don't know if that is the Julian way...
-type SGEArray
-    start::Int
+type SGEArray{T}
+    first::Int
     step::Int
-    array::Array
-    function SGEArray(a::Array)
+    iterator::T
+    state::Any
+    next::Any
+    function SGEArray(it::T)
         if !issgearray()
-            a
+            new(0, 1, it)
         else
-            start = parse(Int, ENV["SGE_TASK_ID"])
+            first = parse(Int, ENV["SGE_TASK_ID"]) - 1
             step = parse(Int, ENV["SGE_TASK_LAST"])
-            l = length(a)
-            if step < start || start > length(a)
-                error("Incorrect array parameters")
+            if step < first
+                error("Incorrect SGE array parameters")
             end
-            new(start, step, a)
+            new(first, step, it)
         end
     end
 end
+## There is something about inner constructors, they need this:
+SGEArray{T}(it::T) = SGEArray{T}(it)
